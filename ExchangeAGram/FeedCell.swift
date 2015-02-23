d9ae2dc30a71bdd6c15acb7414158034e88f6f89
benblanchard <benblanchard22@gmail.com>
2015-02-23 05:55:51 +0000

    Initial Setup of Project, DataSource, FeedCell

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Ben Blanchard on 23/02/2015.
+//  Copyright (c) 2015 Ben Blanchard. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    
+}
---
+++ ExchangeAGram/FeedViewController.swift
@@ -0,0 +1,53 @@
+//
+//  FeedViewController.swift
+//  ExchangeAGram
+//
+//  Created by Ben Blanchard on 23/02/2015.
+//  Copyright (c) 2015 Ben Blanchard. All rights reserved.
+//
+
+import UIKit
+
+//So when we add DataSource and Delegate in here, we need to hook up our collectionView to our FeedViewController in Main.Storyboard
+class FeedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    // MARK: - UICollectionViewDataSource
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+}
